23510504592afde762ff6a7590ccc1b7cd6dd225
Linto <linto.cet@gmail.com>
2023-03-31 13:55:18 +0530

    Update testlink.ts
    remove unwanted by mistake edits

--- src/testlink.ts
+++ src/testlink.ts
@@ -4,7 +4,7 @@
  *
  * testlink-xmlrpc is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, orF
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * testlink-xmlrpc is distributed in the hope that it will be useful,
